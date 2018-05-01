fa3ba733459d2b8b325ff995e3a466b9e2e05352
Edilon Andrade <edilonandrade@gmail.com>
2018-05-01 15:26:53 -0300

    Novos mapas mentais e modificações de existentes.

---
+++ Documentos/MapasMentais/Tipos/CSharpTiposLiterais.mm
@@ -0,0 +1,16 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1525198573219" ID="ID_1653671505" MODIFIED="1525198582477" TEXT="C# - Tipos Literais">
+<node CREATED="1525198637712" ID="ID_539725886" MODIFIED="1525198661945" POSITION="right" TEXT="recebem o tipo do compilador"/>
+<node CREATED="1525198677444" ID="ID_1134029143" MODIFIED="1525198679844" POSITION="left" TEXT="especificar">
+<node CREATED="1525198691629" ID="ID_1702455693" MODIFIED="1525198696974" TEXT="deve ser digitado"/>
+<node CREATED="1525198704870" ID="ID_664627931" MODIFIED="1525198716299" TEXT="anexando uma letra no final do n&#xba;"/>
+</node>
+<node CREATED="1525198729167" ID="ID_378339569" MODIFIED="1525198732751" POSITION="right" TEXT="exemplos">
+<node CREATED="1525198777985" ID="ID_808834548" MODIFIED="1525198780445" TEXT="4.56f"/>
+</node>
+<node CREATED="1525198816792" ID="ID_1070485131" MODIFIED="1525198820336" POSITION="left" TEXT="s&#xe3;o tipados">
+<node CREATED="1525198838225" ID="ID_1197089252" MODIFIED="1525198856076" TEXT="derivam basicamente de System.Object"/>
+</node>
+</node>
+</map>
