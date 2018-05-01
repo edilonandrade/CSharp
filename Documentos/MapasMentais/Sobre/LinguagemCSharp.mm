d8c7c209e0aacb4ef221f416586507340a29d221
Edilon Andrade <edilonandrade@gmail.com>
2018-05-01 14:13:32 -0300

    Adicionando arquivos de mapas mentais.

---
+++ Documentos/MapasMentais/Sobre/LinguagemCSharp.mm
@@ -0,0 +1,134 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1525152723140" ID="ID_1445181314" MODIFIED="1525153851472" TEXT="C#">
+<edge STYLE="sharp_bezier" WIDTH="2"/>
+<node COLOR="#0000ff" CREATED="1525152731589" ID="ID_943546607" MODIFIED="1525153994613" POSITION="right" TEXT="linguagem fortemente tipada">
+<edge STYLE="sharp_bezier" WIDTH="2"/>
+</node>
+<node CREATED="1525152771999" ID="ID_1363155999" MODIFIED="1525153964963" POSITION="left" TEXT="tem um tipo">
+<edge COLOR="#ffff00" STYLE="sharp_bezier" WIDTH="2"/>
+<node CREATED="1525152775931" ID="ID_635583219" MODIFIED="1525153964964" TEXT="vari&#xe1;veis">
+<edge STYLE="sharp_bezier" WIDTH="2"/>
+</node>
+<node CREATED="1525152782732" ID="ID_80882152" MODIFIED="1525153964963" TEXT="constantes">
+<edge STYLE="sharp_bezier" WIDTH="2"/>
+</node>
+<node CREATED="1525152787404" ID="ID_1894795154" MODIFIED="1525153964964" TEXT="express&#xe3;o">
+<edge STYLE="sharp_bezier" WIDTH="2"/>
+</node>
+<node CREATED="1525152813162" ID="ID_1558660242" MODIFIED="1525153964964" TEXT="par&#xe2;metros de entrada em m&#xe9;todos">
+<edge STYLE="sharp_bezier" WIDTH="2"/>
+</node>
+</node>
+<node CREATED="1525152846014" ID="ID_1600137903" MODIFIED="1525154003989" POSITION="right" TEXT="tipos">
+<edge COLOR="#33ff33" STYLE="sharp_bezier" WIDTH="2"/>
+<node CREATED="1525152885655" ID="ID_1000991465" MODIFIED="1525153851472" TEXT="n&#xfa;mericos internos">
+<edge STYLE="sharp_bezier" WIDTH="2"/>
+</node>
+<node CREATED="1525152891564" ID="ID_1246821689" MODIFIED="1525153851472" TEXT="mais complexos">
+<edge STYLE="sharp_bezier" WIDTH="2"/>
+<node CREATED="1525152954791" ID="ID_950597004" MODIFIED="1525153851472" TEXT="ampla variedades">
+<edge STYLE="sharp_bezier" WIDTH="2"/>
+</node>
+<node CREATED="1525152976441" ID="ID_1030451557" MODIFIED="1525153851472" TEXT="construtores l&#xf3;gicos">
+<edge STYLE="sharp_bezier" WIDTH="2"/>
+<node CREATED="1525152989951" ID="ID_601762212" MODIFIED="1525153851472" TEXT="sistema de arquivos">
+<edge STYLE="sharp_bezier" WIDTH="2"/>
+</node>
+<node CREATED="1525152995234" ID="ID_1460970545" MODIFIED="1525153851471" TEXT="conex&#xf5;es de rede">
+<edge STYLE="sharp_bezier" WIDTH="2"/>
+</node>
+<node CREATED="1525153028050" ID="ID_544858180" MODIFIED="1525153851471" TEXT="cole&#xe7;&#xf5;es">
+<edge STYLE="sharp_bezier" WIDTH="2"/>
+</node>
+<node CREATED="1525153032254" ID="ID_694468415" MODIFIED="1525153851471" TEXT="matrizes de objetos">
+<edge STYLE="sharp_bezier" WIDTH="2"/>
+</node>
+<node CREATED="1525153046010" ID="ID_797469259" MODIFIED="1525153851471" TEXT="datas">
+<edge STYLE="sharp_bezier" WIDTH="2"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1525153346093" ID="ID_1749024275" MODIFIED="1525153980084" POSITION="left" TEXT="informa&#xe7;&#xf5;es em um tipo">
+<edge COLOR="#ff0000" STYLE="sharp_bezier" WIDTH="2"/>
+<node CREATED="1525153364782" ID="ID_1533433582" MODIFIED="1525153851471" TEXT="espa&#xe7;o de armazenamento">
+<edge STYLE="sharp_bezier" WIDTH="2"/>
+</node>
+<node CREATED="1525153386317" ID="ID_1965094618" MODIFIED="1525153851471" TEXT="valores">
+<edge STYLE="sharp_bezier" WIDTH="2"/>
+<node CREATED="1525153388479" ID="ID_773049927" MODIFIED="1525153851471" TEXT="minimo">
+<edge STYLE="sharp_bezier" WIDTH="2"/>
+</node>
+<node CREATED="1525153391822" ID="ID_1559384361" MODIFIED="1525153851471" TEXT="m&#xe1;ximo">
+<edge STYLE="sharp_bezier" WIDTH="2"/>
+</node>
+</node>
+<node CREATED="1525153402237" ID="ID_1845137529" MODIFIED="1525153851471" TEXT="menbros">
+<edge STYLE="sharp_bezier" WIDTH="2"/>
+<node CREATED="1525153406037" ID="ID_980296236" MODIFIED="1525153851471" TEXT="m&#xe9;todos">
+<edge STYLE="sharp_bezier" WIDTH="2"/>
+</node>
+<node CREATED="1525153415465" ID="ID_730374454" MODIFIED="1525153851471" TEXT="campos">
+<edge STYLE="sharp_bezier" WIDTH="2"/>
+</node>
+<node CREATED="1525153421153" ID="ID_1431387653" MODIFIED="1525153851471" TEXT="eventos">
+<edge STYLE="sharp_bezier" WIDTH="2"/>
+</node>
+<node CREATED="1525153424317" ID="ID_343107261" MODIFIED="1525153851471" TEXT="etc">
+<edge STYLE="sharp_bezier" WIDTH="2"/>
+</node>
+</node>
+<node CREATED="1525153465175" ID="ID_245564270" MODIFIED="1525153851471" TEXT="tipo base do qual herda">
+<edge STYLE="sharp_bezier" WIDTH="2"/>
+</node>
+<node CREATED="1525153477303" ID="ID_273540684" MODIFIED="1525153851471" TEXT="local na mem&#xf3;ria">
+<edge STYLE="sharp_bezier" WIDTH="2"/>
+<node CREATED="1525153501957" ID="ID_562011210" MODIFIED="1525153851471" TEXT="para vair&#xe1;veis">
+<edge STYLE="sharp_bezier" WIDTH="2"/>
+</node>
+<node CREATED="1525153506878" ID="ID_1939934173" MODIFIED="1525153851471" TEXT="alocada em tempo de execu&#xe7;&#xe3;o">
+<edge STYLE="sharp_bezier" WIDTH="2"/>
+</node>
+</node>
+<node CREATED="1525153523596" ID="ID_1000959347" MODIFIED="1525153851471" TEXT="tipos de opera&#xe7;&#xf5;es permitidos">
+<edge STYLE="sharp_bezier" WIDTH="2"/>
+</node>
+<node CREATED="1525153553358" ID="ID_293483925" MODIFIED="1525153851470" TEXT="utilizadas pelo compilador">
+<edge STYLE="sharp_bezier" WIDTH="2"/>
+<node CREATED="1525153561240" ID="ID_1344819589" MODIFIED="1525153851470" TEXT="para fazer valdia&#xe7;&#xf5;es">
+<edge STYLE="sharp_bezier" WIDTH="2"/>
+</node>
+<node CREATED="1525153576252" ID="ID_1873482896" MODIFIED="1525153851469" TEXT="fortemente tipadas">
+<edge STYLE="sharp_bezier" WIDTH="2"/>
+</node>
+<node CREATED="1525154217047" ID="ID_522695381" MODIFIED="1525154222797" TEXT="insere informa&#xe7;&#xf5;es no exe">
+<node CREATED="1525154238958" HGAP="19" ID="ID_235846135" MODIFIED="1525154253870" TEXT="como metadados" VSHIFT="-39"/>
+</node>
+<node CREATED="1525154271560" ID="ID_1774258656" MODIFIED="1525154274263" TEXT="CLR">
+<node CREATED="1525154289464" HGAP="23" ID="ID_1862590795" MODIFIED="1525154304527" TEXT="Common Language Runtime" VSHIFT="-13"/>
+<node CREATED="1525154327882" ID="ID_1482148140" MODIFIED="1525154338177" TEXT="usa metadados em tempo de execu&#xe7;&#xe3;o"/>
+<node CREATED="1525154383479" ID="ID_829705571" MODIFIED="1525154393355" TEXT="para garantir">
+<node CREATED="1525154413175" ID="ID_923940847" MODIFIED="1525154419976" TEXT="mais seguran&#xe7;a nos tipos"/>
+<node CREATED="1525154425815" ID="ID_1762849611" MODIFIED="1525154433695" TEXT="qdo aloca e recupera mem&#xf3;ria"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1525153110378" ID="ID_959801480" MODIFIED="1525154019295" POSITION="right" TEXT="programa em C# usa">
+<edge COLOR="#663300" STYLE="sharp_bezier" WIDTH="2"/>
+<node CREATED="1525153184530" ID="ID_1735109353" MODIFIED="1525153851469" TEXT="tipos da biblioteca de classes">
+<edge STYLE="sharp_bezier" WIDTH="2"/>
+</node>
+<node CREATED="1525153188074" ID="ID_116737012" MODIFIED="1525153851469" TEXT="tipos definidos pelo usu&#xe1;rio">
+<edge STYLE="sharp_bezier" WIDTH="2"/>
+<node CREATED="1525153262853" ID="ID_78708501" MODIFIED="1525153851469" TEXT="modelam os conceitos">
+<edge STYLE="sharp_bezier" WIDTH="2"/>
+</node>
+<node CREATED="1525153282801" ID="ID_958091697" MODIFIED="1525153851469" TEXT="espec&#xed;ficos ao dominio">
+<edge STYLE="sharp_bezier" WIDTH="2"/>
+</node>
+</node>
+</node>
+</node>
+</map>
