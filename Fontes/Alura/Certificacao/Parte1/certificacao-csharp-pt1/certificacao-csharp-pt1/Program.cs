5a748e4d32ffbfe43164174c2c08b546b8690e02
Edilon Andrade <edilonandrade@gmail.com>
2020-08-26 21:54:57 -0300

    Exclusão de arquivo antigo.

---
+++ Fontes/Alura/Certificacao/Parte1/certificacao-csharp-pt1/certificacao-csharp-pt1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_csharp_pt1
+{
+    class Program
+    {
+        public Program()
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+        }
+    }
+}
