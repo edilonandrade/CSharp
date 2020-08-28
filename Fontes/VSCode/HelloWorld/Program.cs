9a8e46632257c01633c55285e6b659a9e9aa14c0
Edilon Andrade <edilonandrade@gmail.com>
2020-08-28 19:32:02 -0300

    Exemplo com nome e data.

--- Fontes/VSCode/HelloWorld/Program.cs
+++ Fontes/VSCode/HelloWorld/Program.cs
@@ -6,7 +6,13 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("\nWhat is your name?");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+
+            System.Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
+            System.Console.WriteLine("\nPress any Key to exit...");
+            Console.ReadKey(true);
         }
     }
 }
